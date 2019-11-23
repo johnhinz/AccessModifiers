00eb11629d73adc9c014e8b557a000e09d709182
unknown <jhinz402@hotmail.com>
2019-11-22 23:00:48 -0500

    Initial Commit

---
+++ Animals/Animals.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Animals
+{
+    public class Animal
+    {
+        // public - most accessible - anywhere you have an instance of Animal
+        public int NumberOfLegs { get; set; }
+
+        // private - least accessible - only this class
+        private TimeSpan Age { get; set; }
+
+        // protected - accesible from this class and derived classes
+        protected string Sound { get; set; }
+
+        //internal - accessed by any code in the same assembly, but not from another assembly.
+        internal int NumberOfEyes { get; set; }
+    }
+
+    public class Dog : Animal
+    {
+        public Dog()
+        {
+            NumberOfLegs = 4;
+            // Age - not accessible
+            Sound = "Bark";
+            NumberOfEyes = 2;
+        }
+    }
+
+}
+
+
+
---
+++ People/Person.cs
@@ -0,0 +1,17 @@
+﻿using Animals;
+using System;
+
+namespace People
+{
+    public class Person : Animal
+    {
+        public Person()
+        {
+            NumberOfLegs = 2;
+            //not accessible - internal- NumberOfEyes = 2;
+            Sound = "Language";
+            NumberOfEars = 2;
+
+        }
+    }
+}
